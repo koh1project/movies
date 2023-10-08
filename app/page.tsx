7d78044ff2be8b11e961506ee0dd422bd00e8606
Koichi <koichi89592@gmail.com>
2023-10-07 18:52:12 -0700

    Add Fluent UI library and replace some components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import Image from "next/image";
+"use client";
+
+import { Image, Link, Stack, Text } from "@fluentui/react";
 
 import styles from "./page.module.css";
 
@@ -6,88 +8,77 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
+        <Text block> Get started by editing&nbsp;</Text>
+        <Stack>
+          <Link
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            <Text>By</Text>
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
               height={24}
-              priority
             />
-          </a>
-        </div>
+          </Link>
+        </Stack>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Stack className={styles.center}>
+        <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} />
+      </Stack>
 
       <div className={styles.grid}>
-        <a
+        <Link
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
+          <Text as={"h2"}>
             Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          </Text>
+          <Text>Find in-depth information about Next.js features and API.</Text>
+        </Link>
 
-        <a
+        <Link
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
+          <Text as={"h2"}>
             Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+          </Text>
+          <Text>Learn about Next.js in an interactive course with&nbsp;quizzes!</Text>
+        </Link>
 
-        <a
+        <Link
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
+          <Text as={"h2"}>
             Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+          </Text>
+          <Text>Explore the Next.js 13 playground.</Text>
+        </Link>
 
-        <a
+        <Link
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
+          <Text as={"h2"}>
             Deploy <span>-&gt;</span>
-          </h2>
-          <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
-        </a>
+          </Text>
+          <Text>Instantly deploy your Next.js site to a shareable URL with Vercel.</Text>
+        </Link>
       </div>
     </main>
   );
